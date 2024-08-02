42b0f42a4ac3b1685880e328dd65c4236ff2d858
elechacked <elec.hacked@gmail.com>
2024-08-02 20:49:53 +0530

    Update iframe_mobile.dart
    fixed the issues with webviewcontroller and webviewwidget  
    working fine  with webview flutter: 2.8.0

--- lib/src/widgets/iframe_mobile.dart
+++ lib/src/widgets/iframe_mobile.dart
@@ -1,19 +1,16 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/html_parser.dart';
-import 'package:flutter_html/src/navigation_delegate.dart';
-import 'package:flutter_html/src/replaced_element.dart';
-import 'package:flutter_html/style.dart';
 import 'package:webview_flutter/webview_flutter.dart' as webview;
 import 'package:html/dom.dart' as dom;
+import 'package:flutter_html/style.dart';
+import 'package:flutter_html/html_parser.dart';
+import 'package:flutter_html/src/navigation_delegate.dart' as html_navigation;
+import 'package:flutter_html/src/replaced_element.dart';
 
-/// [IframeContentElement is a [ReplacedElement] with web content.
 class IframeContentElement extends ReplacedElement {
   final String? src;
   final double? width;
   final double? height;
-  final NavigationDelegate? navigationDelegate;
+  final html_navigation.NavigationDelegate? navigationDelegate;
   final UniqueKey key = UniqueKey();
 
   IframeContentElement({
@@ -31,31 +28,25 @@ class IframeContentElement extends ReplacedElement {
     return Container(
       width: width ?? (height ?? 150) * 2,
       height: height ?? (width ?? 300) / 2,
-      child: ContainerSpan(
-        style: context.style,
-        newContext: context,
-        child: webview.WebViewWidget(controller: webview.WebViewController(),
-          // controller:  src,
-          // initialUrl: src,
-          key: key,
-          // javascriptMode: sandboxMode == null || sandboxMode == "allow-scripts"
-          //   ? webview.JavascriptMode.unrestricted
-          //   : webview.JavascriptMode.disabled,
-        // navigationDelegate: (request) async {
-        //   final result = await navigationDelegate!(NavigationRequest(
-        //     url: request.url,
-        //     isForMainFrame: request.isForMainFrame,
-        //   ));
-        //   if (result == NavigationDecision.prevent) {
-        //     return webview.NavigationDecision.prevent;
-        //   } else {
-        //     return webview.NavigationDecision.navigate;
-        //   }
-        // },
-        //   gestureRecognizers: {
-        //     Factory<VerticalDragGestureRecognizer>(() => VerticalDragGestureRecognizer())
-        //   },
-        ),
+      child: webview.WebView(
+        initialUrl: src,
+        javascriptMode: sandboxMode == null || sandboxMode == "allow-scripts"
+            ? webview.JavascriptMode.unrestricted
+            : webview.JavascriptMode.disabled,
+        navigationDelegate: (webview.NavigationRequest request) async {
+          if (navigationDelegate != null) {
+            final result = await navigationDelegate!(html_navigation.NavigationRequest(
+              url: request.url,
+              isForMainFrame: request.isForMainFrame,
+            ));
+            if (result == html_navigation.NavigationDecision.prevent) {
+              return webview.NavigationDecision.prevent;
+            } else {
+              return webview.NavigationDecision.navigate;
+            }
+          }
+          return webview.NavigationDecision.navigate;
+        },
       ),
     );
   }
